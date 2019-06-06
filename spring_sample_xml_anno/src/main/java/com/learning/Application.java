0715f023e25a8c85c4aeb9eab46fc20a6baa7672
ext_victorp <ext_victorp@playtika.com>
2019-06-06 17:51:56 +0300

    Best commit after the best commit ever and the best commit

--- spring_sample_xml_anno/src/main/java/com/learning/Application.java
+++ spring_sample_xml_anno/src/main/java/com/learning/Application.java
@@ -1,7 +1,9 @@
 package com.learning;
 
 
+import com.learning.model.Customer;
 import com.learning.service.CustomerService;
+import com.learning.service.CustomerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,13 +12,20 @@ public class Application {
     public static void main(String[] args) {
 
 
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-       // CustomerService service = appContext.getBean("customerService", CustomerService.class);
+        CustomerService service = new CustomerServiceImpl();
 
 
+        System.out.println(service.findAll().get(0).getFirstName());
 
-     //   System.out.println(service.findAll().get(0).getFirstName();
+
+
+//        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//       // CustomerService service = appContext.getBean("customerService", CustomerService.class);
+//
+//
+//
+//     //   System.out.println(service.findAll().get(0).getFirstName();
 
     }
 }
